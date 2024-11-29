7de8812639780efd71e15cde8278db5c700facbf
saruh <saruh@DESKTOP-7NHTR32>
2024-11-30 06:21:35 +1100

    initial

---
+++ SaveMoney.API/Controllers/MoneyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaveMoney.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoneyController : ControllerBase
+    {
+        [HttpGet("balance")]
+        public decimal Balance()
+        {
+            return 87.45M;
+        }
+    }
+}
